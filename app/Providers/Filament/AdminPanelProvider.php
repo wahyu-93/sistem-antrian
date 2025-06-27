9f820426c6f54edb9e30662a449fc95b40e56c01
wahyu93 <wahyu.wahyudi1005@gmail.com>
2025-06-27 14:16:08 +0800

    ubah name sidebar dari laravel ke sistem antrian

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandName('Sistem Antrian')
             ->id('admin')
             ->path('admin')
             ->login()
--- resources/views/filament/layouts/base-kiosk.blade.php
+++ resources/views/filament/layouts/base-kiosk.blade.php
@@ -9,7 +9,6 @@
     @filamentScripts
     @filamentStyles
 
-
 </head>
 <body class="flex flex-col min-w-screen min-h-screen bg-gray-100">
     <header class="bg-blue-600 text-white p-4 text-lg">
